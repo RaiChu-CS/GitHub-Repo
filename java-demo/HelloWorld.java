60c46ef1477a123cda2f97b949fbf81f5dc5ba69
tamaROSchi <rairosc@outlook.com>
2026-01-31 18:05:00 -0800

    Update HelloWorld.java: add sum output only

--- java-demo/HelloWorld.java
+++ java-demo/HelloWorld.java
@@ -8,13 +8,8 @@ public class HelloWorld {
         int b = 5;
         
         int sum = a + b;
-        int difference = a - b;
-        int product = a * b;
-        int quotient = a / b;
         
         System.out.println("Sum: " + sum);
-        System.out.println("Difference: " + difference);
-        System.out.println("Product: " + product);
-        System.out.println("Quotient: " + quotient);
+       
     } 
 }
